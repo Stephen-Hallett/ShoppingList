1533a6d3a371527050786cd85035725ec9f641bb
Stephen-Hallett <stevohallett@gmail.com>
2025-04-04 18:45:11 +1300

    fix: Refer to resource group properly

--- infra/deploy/main.tf
+++ infra/deploy/main.tf
@@ -3,14 +3,13 @@ data "azurerm_client_config" "current" {}
 data "azurerm_subscription" "primary" {}
 
 data "azurerm_resource_group" "rg" {
-  name     = "rg-${var.project_id}-${var.env}-eau-001"
-  location = "australiaeast"
+  name = "rg-${var.project_id}-${var.env}-eau-001"
 }
 
 resource "azurerm_storage_account" "sa" {
   name                     = "st${var.project_id}${var.env}eau001"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = data.azurerm_resource_group.rg.name
+  location                 = data.azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
@@ -27,8 +26,8 @@ resource "azurerm_storage_table" "lists" {
 
 resource "azurerm_service_plan" "asp" {
   name                = "asp-${var.project_id}-${var.env}-eau-001"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
   os_type             = var.asp_os_type
   sku_name            = var.asp_sku_name
 }
@@ -36,8 +35,8 @@ resource "azurerm_service_plan" "asp" {
 # Create the web app, pass in the App Service Plan ID
 # resource "azurerm_linux_web_app" "webapp" {
 #   name                = var.app_name
-#   location            = azurerm_resource_group.rg.location
-#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = data.azurerm_resource_group.rg.location
+#   resource_group_name = data.azurerm_resource_group.rg.name
 #   service_plan_id     = azurerm_service_plan.asp.id
 #   https_only          = true
 #   site_config {
