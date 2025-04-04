505bbf69eadb2f4374f65752243de950f1238e32
Stephen-Hallett <stevohallett@gmail.com>
2025-04-04 20:00:31 +1300

    feat: Add container app environment

--- infra/deploy/main.tf
+++ infra/deploy/main.tf
@@ -32,20 +32,10 @@ resource "azurerm_service_plan" "asp" {
   sku_name            = var.asp_sku_name
 }
 
-# Create the web app, pass in the App Service Plan ID
-# resource "azurerm_linux_web_app" "webapp" {
-#   name                = var.app_name
-#   location            = data.azurerm_resource_group.rg.location
-#   resource_group_name = data.azurerm_resource_group.rg.name
-#   service_plan_id     = azurerm_service_plan.asp.id
-#   https_only          = true
-#   site_config {
+resource "azurerm_container_app_environment" "containerenv" {
+  name                = "cae-${var.project_id}-${var.env}-eau-001"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
 
-#   }
-# }
 
-# resource "azurerm_role_assignment" "table-contrib" {
-#   scope                = azurerm_storage_account.sa.id
-#   role_definition_name = "Storage Table Data Contributor"
-#   principal_id         = var.user_object_id
-# }
