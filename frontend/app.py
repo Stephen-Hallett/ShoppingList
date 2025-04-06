5faf51513dcbf63124b739a777b6c1953747fdcc
Stephen-Hallett <stevohallett@gmail.com>
2025-04-06 15:09:08 +1200

    chore: Change streamlit app so I can find the right endpoint

--- frontend/app.py
+++ frontend/app.py
@@ -3,4 +3,8 @@ import requests
 import os
 
 st.header("What up again big dog")
-st.write(requests.get(f"{os.environ["backend_endpoint"]}/test").text)
+test = st.text_input("Endpoint")
+
+st.write(f"{os.environ["backend_endpoint"]}/test")
+st.write(f"{test}/test")
+st.write(requests.get(f"{test}/test").text)
